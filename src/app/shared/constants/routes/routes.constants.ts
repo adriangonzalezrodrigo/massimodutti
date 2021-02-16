8819a8b8a4c7b3c8918bafb18cccb83b7fdddd45
Adrián González <agrodrigo@minsait.com>
2021-02-16 20:42:23 +0100

    feat(routes.constants): added app routes

---
+++ src/app/shared/constants/routes/routes.constants.ts
@@ -0,0 +1,26 @@
+export const APP_ROUTES = {
+  ROOT: {
+    path: '',
+  },
+  LOGIN: {
+    path: 'login',
+  },
+  REGISTER: {
+    path: 'register',
+  },
+  PRINCIPAL: {
+    path: 'principal',
+  },
+  SHIPS: {
+    path: 'ships'
+  },
+  PAGE_ONE: {
+    path: 'pageOne'
+  },
+  PAGE_TWO: {
+    path: 'pageTwo'
+  },
+  WILDCARD: {
+    path: '**'
+  }
+};
