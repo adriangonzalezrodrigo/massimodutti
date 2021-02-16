c97f686a9f74ecebb45974b11293a03092941b2b
Adrián González <adrian.gonzalez.rodrigo@gmail.com>
2021-02-16 20:26:46 +0100

    feat(app-routing.module): lazy load modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// Components
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: 'principal', loadChildren: () => import(`./components/principal/principal.module`).then(m => m.PrincipalModule) }
-  // { path: 'ships', loadChildren: () => import(`./components/ships/ships.module`).then(m => m.ShipsModule) }
+  {
+    path: '',
+    loadChildren: () => import(`./modules/login/login.module`).then((m) => m.LoginModule),
+  },
+  { path: 'register',
+  loadChildren: () => import(`./modules/register/register.module`).then((m) => m.RegisterModule), },
+  {
+    path: 'principal',
+    loadChildren: () => import(`./modules/principal/principal.module`).then((m) => m.PrincipalModule),
+  },
+  {
+    path: '**',
+    redirectTo: '' // TODO: Replace with Page Not Found
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
