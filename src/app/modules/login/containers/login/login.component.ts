7f1631d3262965949b98238ad388449d8bc564bb
Adrián González <adrian.gonzalez.rodrigo@gmail.com>
2021-02-17 22:01:16 +0100

    feat(login.component): added

--- src/app/modules/login/containers/login/login.component.ts
+++ src/app/modules/login/containers/login/login.component.ts
@@ -1,45 +1,83 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
-// JSON
-import usersList from 'src/assets/json/users.json';
+import { Subject } from 'rxjs';
+import { finalize, takeUntil } from 'rxjs/operators';
+import { APP_ROUTES } from 'src/app/shared';
+import { USER_VALIDATIONS } from 'src/app/shared/constants';
+import { FireAuthService } from 'src/app/shared/services';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  public loginForm: FormGroup = null;
+  public dataLoading = false;
+  public unregistered = false;
 
-  loginForm: FormGroup;
-  dataLoading: boolean = false;
-  users: any = usersList;
-  unregistered: boolean = false;
-  invalid: boolean = false;
+  invalid = false;
+
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
 
   constructor(
+    private fireAuthService: FireAuthService,
     private fb: FormBuilder,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      username: [ '', [Validators.required, Validators.minLength(3)]],
-      password: [ '', [Validators.required, Validators.minLength(6)]]
-    })
+    this.initFormGroup();
   }
-  loginUser() {
-    if (this.loginForm.invalid) { return }
-    // TODO : Falta integrar el servicio para autentificar al usuario
-    // JSON simulando usuarios
-    var userLogin = this.loginForm.value.username;
-    var filterJson = this.users.filter(function (user) { return user.first_name === userLogin  });
-    if (filterJson.length > 0) {
-      this.router.navigate(['/principal/ships'])
-    } else {
-      this.unregistered = true;
+
+  public ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
+  public loginUser(): void {
+    if (this.loginForm.invalid) {
+      return;
     }
+
+    this.unregistered = false;
+    this.dataLoading = true;
+    this.fireAuthService
+      .signIn(
+        this.loginForm.get('email').value,
+        this.loginForm.get('password').value
+      )
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .pipe(finalize(() => (this.dataLoading = false)))
+      .subscribe((response) => {
+        console.log(response);
+        if (response) {
+          this.router.navigate([
+            `${APP_ROUTES.PRINCIPAL.path}/${APP_ROUTES.SHIPS.path}`,
+          ]);
+        } else {
+          this.unregistered = true;
+        }
+      });
+  }
+
+  private initFormGroup(): void {
+    this.loginForm = this.fb.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(USER_VALIDATIONS.EMAIL.MIN_LENGTH),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(USER_VALIDATIONS.PASSWORD.MIN_LENGTH),
+        ],
+      ],
+    });
   }
 }
-
--- src/app/modules/login/containers/login/specs/login.component.spec.ts
+++ src/app/modules/login/containers/login/specs/login.component.spec.ts
@@ -1,7 +1,10 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { APP_ROUTES } from 'src/app/shared/constants';
+import { FireAuthService } from 'src/app/shared/services';
+import { FireAuthServiceMock } from '../../../../../shared/services/fire-auth/specs/fire-auth.mock';
 import { LoginComponent } from '../login.component';
 
 describe('LoginComponent', () => {
@@ -15,8 +18,17 @@ describe('LoginComponent', () => {
         declarations: [LoginComponent],
         imports: [
           FormsModule,
-          RouterTestingModule.withRoutes([]),
+          RouterTestingModule.withRoutes([
+            {
+              path: `${APP_ROUTES.PRINCIPAL.path}/${APP_ROUTES.SHIPS.path}`,
+              redirectTo: '',
+              pathMatch: 'full',
+            },
+          ]),
           ReactiveFormsModule,
+        ],
+        providers: [
+          { provide: FireAuthService, useClass: FireAuthServiceMock },
         ],
       }).compileComponents();
     })
@@ -24,12 +36,41 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
-
+    router = TestBed.inject(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('initFormGroup should initialize form group', () => {
+    component['initFormGroup']();
+
+    expect(
+      component.loginForm.get('email') instanceof FormControl
+    ).toBeTruthy();
+    expect(
+      component.loginForm.get('password') instanceof FormControl
+    ).toBeTruthy();
+  });
+
+  it('registerUser should calls signUp service method when login form is valid', () => {
+    spyOn<any>(component['fireAuthService'], 'signIn').and.callThrough();
+    const navigateSpy = spyOn(router, 'navigate');
+    component.loginForm.setValue({
+      email: 'email@mail.com',
+      password: 'password',
+    });
+
+    component.loginUser();
+
+    expect(component.loginForm.valid).toBeTruthy();
+    expect(navigateSpy).toHaveBeenCalled();
+    expect(component['fireAuthService'].signIn).toHaveBeenCalledWith(
+      'email@mail.com',
+      'password'
+    );
   });
 });
