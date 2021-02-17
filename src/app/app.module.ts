90d76a4d3f20b9ba730ccb0aa0a993fa2573350a
Adrián González <adrian.gonzalez.rodrigo@gmail.com>
2021-02-17 21:57:28 +0100

    feat(app.module): added firebase  and fire auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
-// Components
 import { AppComponent } from './app.component';
+import { FireAuthService } from './shared';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -13,8 +18,11 @@ import { AppComponent } from './app.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
-  providers: [],
+  providers: [FireAuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
