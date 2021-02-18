c4fb2c43937141cb554cc451a0ca547a943d90e9
Adrián González <adrian.gonzalez.rodrigo@gmail.com>
2021-02-18 19:53:36 +0100

    feat(ships.service): add get ships params

--- src/app/services/ships.service.ts
+++ src/app/services/ships.service.ts
@@ -1,27 +1,27 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators'
+import { map } from 'rxjs/operators';
+import { ShipsAPIResponse } from './models/ships-api-response.model';
+import { ShipsParams } from './models/ships-params.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShipsService {
-
-  url: string = 'https://swapi.dev/api/starships/'
-  headerDict = {
-    'Authorization': 'none',
-    'Access-Control-Allow-Origin': '*'
-  }
-  requestOptions = {                                                                                                                                                                                 
-    headers: new HttpHeaders(this.headerDict), 
+  private url = 'https://swapi.dev/api/starships/';
+  private headerDict = {
+    Authorization: 'none',
+    'Access-Control-Allow-Origin': '*',
   };
-  
-  constructor( private http: HttpClient ) {}
+  private requestOptions = {
+    headers: new HttpHeaders(this.headerDict),
+  };
 
-  getShips(): Observable<any>{
-    return this.http.get(this.url).pipe( 
-      map( data => { return data })
-      );
+  constructor(private http: HttpClient) {}
+
+  public getShips(shipsParams: ShipsParams): Observable<ShipsAPIResponse> {
+    const params = new HttpParams().append('page', shipsParams.page.toString());
+    return this.http.get<ShipsAPIResponse>(this.url, { params });
   }
 }
