9bc453669c182979af1f519320883be806f9233e
Adrián González <adrian.gonzalez.rodrigo@gmail.com>
2021-02-18 19:58:07 +0100

    feat(app.module): added ngrx config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FireAuthService } from './shared';
 
+// ngrx related imports
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { metaReducers, reducers } from './store';
+import { ShipsEffects } from './store/effects';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -21,6 +27,11 @@ import { FireAuthService } from './shared';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
+    // ngrx related imports
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+    }),
+    EffectsModule.forRoot([ShipsEffects]),
   ],
   providers: [FireAuthService],
   bootstrap: [AppComponent],
